a5edcd28e77032801a50d37321e9144843c57f12
Eric.Chou <zcz3313@sina.com>
2021-10-11 17:22:47 +0800

    exit with err-code when failed
    exit with err-code when failed

--- gerrit_entrypoint.sh
+++ gerrit_entrypoint.sh
@@ -11,5 +11,13 @@ rm -rf $gerrit_out_dir
 rm -rf $out_html_dir
 
 $script_path/gerrit_downloader.sh $*
+if [ $? != 0 ]
+then
+  exit $?
+fi
 
 $script_path/gerrit_stats.sh -f $gerrit_out_dir
+if [ $? != 0 ]
+then
+  exit $?
+fi
