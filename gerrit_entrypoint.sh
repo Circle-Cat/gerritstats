c09a023011a4a677762d46528843f5842c24faf0
Eric.Chou <zcz3313@sina.com>
2021-10-11 09:56:29 +0800

    Create gerrit_entrypoint.sh
    add gerrit_entrypoint.sh

---
+++ gerrit_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Run GerritDownloader and GerritStats.
+#
+
+script_path="$(cd "$(dirname -- "$0")" || exit 1; pwd -P)"
+gerrit_out_dir="$script_path/gerrit_out"
+out_html_dir="$script_path/out-html"
+
+rm -rf $gerrit_out_dir
+rm -rf $out_html_dir
+
+$script_path/gerrit_downloader.sh $*
+
+$script_path/gerrit_stats.sh -f $gerrit_out_dir
